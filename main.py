7898c74b4f4462af053928e6e190b76a7e4c85b6
Mark Solante <marksolante221@gmail.com>
2024-12-15 17:20:48 +0800

    [UPDATE 0.0.2 : Added Game Background]

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Testing")
 
+BG_IMG = pygame.transform.scale(pygame.image.load("./assets/bg.jpg"), (WIDTH, HEIGHT))
+
+def draw():
+    WIN.blit(BG_IMG, (0,0))
+    pygame.display.update()
 
 def main():
     run = True
@@ -14,6 +19,8 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        
+        draw()
 
     pygame.quit()
 
